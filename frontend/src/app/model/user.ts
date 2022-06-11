ed03824e1bb6bd393331175fe7e0223a45b2f367
Daniel Kovacs <info@kovix.info>
2022-06-11 11:01:52 +0200

    create user model

---
+++ frontend/src/app/model/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  _id: string = "";
+  userName: string = "";
+  password: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  createdAt: string = "";
+  updatedAt: string = "";
+}
+
