6e478b8badaf7b603cb259db8cc4564c95129e83
Daniel Kovacs <info@kovix.info>
2022-06-11 11:08:53 +0200

    create patient model

---
+++ frontend/src/app/model/patient.ts
@@ -0,0 +1,14 @@
+export class Patient {
+  _id: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  patientId?: string;
+  email?: string;
+  comment?: string;
+  createdBy: string = "";
+  deletedBy?: string;
+  deletedAt: string = "";
+  createdAt: string = "";
+  updatedAt: string = "";
+}
+
