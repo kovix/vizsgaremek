f9df6e681716794a1cfa542350da6d42cf62fc1c
Daniel Kovacs <info@kovix.info>
2022-06-11 11:10:50 +0200

    crate examination model

---
+++ frontend/src/app/model/examination.ts
@@ -0,0 +1,9 @@
+export class Examination {
+  _id: string = "";
+  name: string = "";
+  defaultTime: number = 0;
+  createdBy: string = "";
+  deletedBy?: string;
+  createdAt: string = "";
+  updatedAt: string = "";
+}
