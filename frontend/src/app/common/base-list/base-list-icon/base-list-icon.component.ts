3f6eed0fffc00c64c900142e9466b3d2644cb807
Daniel Kovacs <info@kovix.info>
2022-06-19 18:15:50 +0200

    change litner settings

--- frontend/src/app/common/base-list/base-list-icon/base-list-icon.component.ts
+++ frontend/src/app/common/base-list/base-list-icon/base-list-icon.component.ts
@@ -1,17 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-base-list-icon',
   templateUrl: './base-list-icon.component.html',
   styleUrls: ['./base-list-icon.component.scss']
 })
-export class BaseListIconComponent implements OnInit {
+export class BaseListIconComponent  {
 
   @Input() direction: string = '';
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
--- frontend/src/app/common/base-list/base-list-row/base-list-row.component.ts
+++ frontend/src/app/common/base-list/base-list-row/base-list-row.component.ts
@@ -1,27 +1,22 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Entity } from 'src/app/model/entity';
 import { ButtonDefinition } from 'src/app/model/genericTable/button-definition';
 import { ColumnDefinition } from 'src/app/model/genericTable/column-definition';
 import { CustomButtonEvent } from 'src/app/model/genericTable/custom-button-event';
 
-
+/* eslint-disable-next-line */
 @Component({
   selector: '[app-base-list-row]',
   templateUrl: './base-list-row.component.html',
   styleUrls: ['./base-list-row.component.scss']
 })
-export class BaseListRowComponent<GenericEntity extends Entity>  implements OnInit {
+export class BaseListRowComponent<GenericEntity extends Entity> {
 
   @Input() columnDefinition: ColumnDefinition[] = [];
   @Input() entity!: GenericEntity;
   @Input() actionButtons: ButtonDefinition[] = [];
 
   @Output() customButtonClicked: EventEmitter<CustomButtonEvent> = new EventEmitter();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   isBooleanColumn(entity: any) {
     return typeof entity === 'boolean';
