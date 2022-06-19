4b95d32f059bcf5699e1ef889be26a5fda5a38dd
Daniel Kovacs <info@kovix.info>
2022-06-19 20:23:53 +0200

    [BACKEND] create examinationgroup model

---
+++ backend/src/model/examinationGroup.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const softDelete = require('mongoose-delete');
+const idvalidator = require('mongoose-id-validator');
+
+const examinationGroupSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  examinations: [
+    {
+      order: {
+        type: Number,
+        required: true,
+      },
+      examination: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'examinations',
+        required: true,
+      },
+    },
+  ],
+}, {
+  timestamps: true,
+});
+
+examinationGroupSchema.plugin(idvalidator);
+
+module.exports = mongoose.model('Examination', examinationGroupSchema);
