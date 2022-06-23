b50ad48e58930d86a6f60efc713185c09ebb2632
Daniel Kovacs <info@kovix.info>
2022-06-23 15:23:10 +0200

    Fix gilter

--- frontend/src/app/pipe/filter.pipe.ts
+++ frontend/src/app/pipe/filter.pipe.ts
@@ -9,7 +9,7 @@ export class FilterPipe implements PipeTransform {
     private countCommunicator: CountReporterService
   ) { }
 
-  transform<T extends {[key: string]: any}>(value: T[]|null, phrase: string, activeValue: boolean, featuredValue: boolean, listTitle: string|null): T[]|null {
+  transform<T extends {[key: string]: any}>(value: T[]|null, phrase: string, listTitle: string|null): T[]|null {
     const reportTitle = listTitle || '';
     if (!Array.isArray(value) || value.length === 0 ) {
       this.countCommunicator.reportCount(reportTitle, 0);
@@ -18,29 +18,29 @@ export class FilterPipe implements PipeTransform {
 
     let filteredValue: T[] = value;
 
-    if (value[0].hasOwnProperty('active')) {
-      filteredValue = value.filter(val => val['active'] === activeValue);
-    }
-
-    if (value[0].hasOwnProperty('featured')) {
-      filteredValue = filteredValue.filter(val => val['featured'] === featuredValue);
-    }
-
     const ret = filteredValue.filter((entity) => {
-      let numberPhrase = parseInt(phrase);
-      if (!isNaN(numberPhrase)) {
-        for (const [key, value] of Object.entries(entity)) {
-          if (value === numberPhrase) return true;
+
+      for (const [key, value] of Object.entries(entity)) {
+        if (typeof value === 'string') {
+          if (value.toLowerCase().includes(phrase.toLowerCase())) return true;
         }
-        return false;
-      } else {
-        for (const [key, value] of Object.entries(entity)) {
-          if (typeof value === 'string') {
-            if (value.toLowerCase().includes(phrase.toLowerCase())) return true;
-          }
-        }
-        return false;
       }
+      return false;
+
+      // let numberPhrase = parseInt(phrase);
+      // if (false) {
+      //   for (const [key, value] of Object.entries(entity)) {
+      //     if (value === numberPhrase) return true;
+      //   }
+      //   return false;
+      // } else {
+      //   for (const [key, value] of Object.entries(entity)) {
+      //     if (typeof value === 'string') {
+      //       if (value.toLowerCase().includes(phrase.toLowerCase())) return true;
+      //     }
+      //   }
+      //   return false;
+      // }
     });
     this.countCommunicator.reportCount(reportTitle, ret.length);
     return ret;
