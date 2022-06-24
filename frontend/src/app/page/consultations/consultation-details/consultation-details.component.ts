b83a70fe715e843ac20a2658d0330d2e0aa42d60
Daniel Kovacs <info@kovix.info>
2022-06-24 15:49:35 +0200

    Fix sidenav clickable when dialog is open

--- frontend/src/app/page/consultations/consultation-details/consultation-details.component.ts
+++ frontend/src/app/page/consultations/consultation-details/consultation-details.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, OnDestroy, Renderer2, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-consultation-details',
   templateUrl: './consultation-details.component.html',
   styleUrls: ['./consultation-details.component.scss']
 })
-export class ConsultationDetailsComponent implements OnInit {
+export class ConsultationDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+  ) { }
 
   ngOnInit(): void {
+    debugger;
+    this.renderer.addClass(this.document.body, 'setalo-body');
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(this.document.body, 'setalo-body');
   }
 
 }
